617510c172df368d41d7c8abdebeaf5480c532fc
Laurennc33 <laurennc3323@gmail.com>
2025-04-12 22:48:11 -0400

    80% of a6

--- kambaz/enrollments/dao.js
+++ kambaz/enrollments/dao.js
@@ -1,19 +1,29 @@
-import Database from "../database/index.js";
 import { v4 as uuidv4 } from "uuid";
+import model from "./model.js";
 
-export function enrollUserInCourse(userId, courseId) {
-  const newEnrollment = { _id: uuidv4(), user: userId, course: courseId };
-  Database.enrollments.push(newEnrollment);
-  return newEnrollment; 
+export async function findCoursesForUser(userId) {
+ const enrollments = await model.find({ user: userId }).populate("course");
+ return enrollments.map((enrollment) => enrollment.course);
 }
 
-export function unenrollUserFromCourse(userId, courseId) {
-  Database.enrollments = Database.enrollments.filter(
-    (enrollment) => !(enrollment.user === userId && enrollment.course === courseId)
-  );
-  return { success: true }; 
+export async function findUsersForCourse(courseId) {
+ const enrollments = await model.find({ course: courseId }).populate("user");
+ return enrollments.map((enrollment) => enrollment.user);
 }
 
+export function enrollUserInCourse(user, course) {
+  const newEnrollment = { user, course, _id: `${user}-${course}` };
+  return model.create(newEnrollment);
+}
+
+export function unenrollUserFromCourse(user, course) {
+  return model.deleteOne({ user, course });
+}
+ 
+
+
+
+// Old enroll ment code
 export function getUserEnrollments(userId) {
   return Database.enrollments.filter((e) => e.user === userId);
 }
