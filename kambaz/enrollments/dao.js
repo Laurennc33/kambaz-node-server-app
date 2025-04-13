11b3f1c345cb02d423727e6c796121319696b2d0
Laurennc33 <laurennc3323@gmail.com>
2025-04-13 16:06:12 -0400

    debugged

--- kambaz/enrollments/dao.js
+++ kambaz/enrollments/dao.js
@@ -1,22 +1,26 @@
 import model from "./model.js";
 
 export async function findCoursesForUser(userId) {
- const enrollments = await model.find({ user: userId }).populate("course");
- return enrollments.map((enrollment) => enrollment.course);
+  const enrollments = await model.find({ user: userId }).populate("course");
+  return enrollments.map((enrollment) => enrollment.course);
 }
 
 export async function findUsersForCourse(courseId) {
- const enrollments = await model.find({ course: courseId }).populate("user");
- return enrollments.map((enrollment) => enrollment.user);
+  const enrollments = await model.find({ course: courseId }).populate("user");
+  return enrollments.map((enrollment) => enrollment.user);
 }
 
-export function enrollUserInCourse(user, course) {
-  const newEnrollment = { user, course, _id: `${user}-${course}` };
+export async function enrollUserInCourse(user, course) {
+  const enrollmentId = `${user}-${course}`;
+  const existing = await model.findById(enrollmentId);
+  if (existing) {
+    throw new Error("User is already enrolled in this course.");
+  }
+
+  const newEnrollment = { user, course, _id: enrollmentId };
   return model.create(newEnrollment);
 }
 
 export function unenrollUserFromCourse(user, course) {
   return model.deleteOne({ user, course });
 }
-
- 
